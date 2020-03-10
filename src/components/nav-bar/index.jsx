f2a8f0b54fb5c12f3cbe94124afce47635b98fe0
zhangzhang1 <hzzhangzhang1@corp.netease.com>
2020-03-10 16:33:20 +0800

    feature:订单组件

---
+++ src/components/nav-bar/index.jsx
@@ -0,0 +1,20 @@
+import Taro from '@tarojs/taro'
+import {AtNavBar} from 'taro-ui'
+
+export default function NavBar(props) {
+  // 管理员入口
+  const entryfunc = () => {
+    Taro.navigateTo({
+      url: `/pages/order/index`
+    })
+  }
+
+  return (
+    <AtNavBar
+      onClickRgIconNd={props.entryfunc || entryfunc}
+      color='#000'
+      title={props.title}
+      rightSecondIconType={props.icon||'user'}
+    />
+  )
+}
