b4db28e6cd2f398810a40378dfaf158483cb13d7
zhangzhang1 <hzzhangzhang1@corp.netease.com>
2020-03-12 11:57:15 +0800

    misc:文件精简

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,53 +1,21 @@
 import Taro from "@tarojs/taro";
 
-/**
- * 函数防抖 (只执行最后一次点击)
- */
-export const debounce = (fn, t) => {
-  let delay = t || 500;
-  let timer;
-  return function() {
-    let args = arguments;
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      timer = null;
-      fn.apply(this, args);
-    }, delay);
-  };
-};
-
 // 获取当前页面url
 export const getCurrentPageUrl = () => Taro._$router.path;
 export const queryString = url => {
-  url = url || window.location.href;
-  let reg = /(\w+)=([^&]+)/g,
-    params = {},
-    result = [],
-    index = url.indexOf("?");
+  var reg = /(\w+)=([^&]+)/g,
+        params = {},
+        result = [],
+        url = url || window.location.href;
+    if(url.indexOf('groupmessage') > -1 || url.indexOf('singlemessage') > -1 || url.indexOf('timeline') > -1){
+        url = url.replace(/\?from=(groupmessage|singlemessage|timeline)(\S*)#/, '#');
+    }
+    
+    url = (url.split('?')[1] || '');
 
-  url = decodeURIComponent(url.slice(index + 1, url.length) || "");
-  while ((result = reg.exec(url))) {
-    params[result[1]] = result[2];
-  }
-  return params;
-};
+    while (result = reg.exec(url)) {
+        params[result[1]] = decodeURI(result[2]);
+    }
 
-// 复制文字
-export const copyText = text => {
-  text &&
-    wx.setClipboardData({
-      data: text
-    });
-};
-
-// 处理时间戳-xxxx-yy-dd
-export const getData = (_time = +new Date()) => {
-  let date = new Date(_time + 8 * 3600 * 1000);
-  return date
-    .toJSON()
-    .substr(0, 19)
-    .replace("T", " ")
-    .split(" ")[0];
+    return params;
 };
